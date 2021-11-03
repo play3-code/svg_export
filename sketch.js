ce9b1e71fa21649639d6a4d5cbf897c3df57724f
chrischne <chrischne@gmail.com>
2021-11-03 16:02:10 +0100

    create simple example

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-// animation 001
+// svg_export
 
 let geodata;
 let treeData;
@@ -17,61 +17,34 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(900, 650);
+  createCanvas(900, 650, SVG);
 
   treeData = geodata.features;
   console.log(treeData.length);
 
-  // filter out trees with no PFLANZJAHR
-  treeData = treeData.filter(function (d) {
-    return d.properties.PFLANZJAHR != null;
-  });
-  console.log(treeData.length);
-
-  // filter out obviously invalid year data (12)
-  treeData = treeData.filter(function (d) {
-    return d.properties.PFLANZJAHR > 1000;
-  });
-
-  // get the start year for the animation, this is the earliest year in the data
-  let startYear = d3.min(treeData, function (d) {
-    return d.properties.PFLANZJAHR;
-  });
-
-  console.log("startYear", startYear);
-
-  currentYear = startYear;
-
-  frameRate(30);
+  noLoop();
 }
 
 function draw() {
-  background(255);
-
-  noStroke();
-  fill(0);
-  text(currentYear, 50, 50);
+  clear();
 
   drawTrees();
-
-  currentYear += 0.5;
+  //  save("trees.svg");
 }
 
 function drawTrees() {
   for (let i = 0; i < treeData.length; i++) {
     let treeObject = treeData[i];
 
-    if (treeObject.properties.PFLANZJAHR < currentYear) {
-      let coordinates = treeObject.geometry.coordinates;
-      let lat = coordinates[1];
-      let lon = coordinates[0];
+    let coordinates = treeObject.geometry.coordinates;
+    let lat = coordinates[1];
+    let lon = coordinates[0];
 
-      let x = map(lon, bounds.left, bounds.right, 0, width);
-      let y = map(lat, bounds.top, bounds.bottom, 0, height);
+    let x = map(lon, bounds.left, bounds.right, 0, width);
+    let y = map(lat, bounds.top, bounds.bottom, 0, height);
 
-      fill(0);
-      noStroke();
-      ellipse(x, y, 3, 3);
-    }
+    fill(0, 255, 0);
+    noStroke();
+    ellipse(x, y, 10, 10);
   }
 }
